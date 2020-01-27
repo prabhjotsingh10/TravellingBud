84fe3dfa10df3abdc379446674dac75c226c4acd
prabhjotsingh10 <prabhjot.singh232@gmail.com>
2020-01-26 21:57:49 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+const exphbr = require('express-handlebars');
+
+app.use(express.static('CSS and Images'));
+
+app.engine('handlebars', exphbr());
+app.set('view engine','handlebars');
+
+app.get('/',(req,res) =>{
+    res.render("home", {
+        title:"Welcome to TravellingBud "
+    })
+})
+
+app.listen(4000, () => {
+    console.log("The server is up and running");
+})
