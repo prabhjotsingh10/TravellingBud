8ad1b4451bc48065ef0086182a482f92e60bc863
prabhjotsingh10 <prabhjot.singh232@gmail.com>
2020-02-06 00:49:21 -0500

    added routes, logo and gitignore

--- index.js
+++ index.js
@@ -15,13 +15,37 @@ app.get('/',(req,res) =>{
     })
 })
 
+app.get('/home',(req,res) =>{
+    res.render("home", {
+        title:"Welcome to TravellingBud "
+    })
+})
+
+
 app.get("/registration", (req,res) => {
 
     res.render("registration", {
-        title:"Room"
+        title:"Featured Room Listings",
+        heading: "Featured Rooms Just For You"
     });
 
 })
+
+app.post("/validation", (req,res) =>{
+
+    const errors = [];
+
+    if(req.body.email == "")
+    {
+        errors.push("Sorry, You must enter an email");
+    }
+
+    if(req.body.pass == ""){
+        errors.push("Sorry, You must create a password to continue");
+    }
+})
+
+
 
 app.listen(4000, () => {
     console.log("The server is up and running");
