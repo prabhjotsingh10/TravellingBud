d6430940d8dfae4bef46741e00e48b8077f00339
prabhjotsingh10 <prabhjot.singh232@gmail.com>
2020-02-09 12:25:06 -0500

    First-commit

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const express = require('express');
-
 const app = express();
-
 const exphbr = require('express-handlebars');
-
+const bodyParser = require('body-parser');
 app.use(express.static('CSS and Images'));
-
+app.use(bodyParser.urlencoded({ extended: false }))
 app.engine('handlebars', exphbr());
 app.set('view engine','handlebars');
 
 app.get('/',(req,res) =>{
     res.render("home", {
-        title:"Welcome to TravellingBud "
+        title:"Welcome to TravellingBud ",
+
     })
 })
 
@@ -31,6 +30,25 @@ app.get("/registration", (req,res) => {
 
 })
 
+app.get("/login", (req,res) => {
+
+    res.render("login", {
+        title:"Log In",
+       
+    });
+
+})
+
+app.get("/sign_up", (req,res) => {
+
+    res.render("sign_up", {
+        title:"Sign Up Here",
+       
+    });
+
+})
+
+
 app.post("/validation", (req,res) =>{
 
     const errors = [];
@@ -43,6 +61,70 @@ app.post("/validation", (req,res) =>{
     if(req.body.pass == ""){
         errors.push("Sorry, You must create a password to continue");
     }
+
+    if(errors.length > 0){
+        res.render("login",
+        {
+            messages : errors
+        })
+    }
+
+})
+
+app.post("/validate-home", (req,res)=>{
+
+    const errors=[];
+
+    if(req.body.check_in.value == undefined){
+        errors.push("Please Enter a date for check in");
+        console.log(errors);
+    }
+
+    if(req.body.check_out.value == undefined){
+        errors.push("Please Enter a date for check out");
+    }
+
+    if(errors.length > 0 )
+    res.render("home",{
+        messages:errors
+    }
+    )
+})
+
+app.post("/validate_sign_up", (req,res)=>{
+
+    const errors=[];
+
+    if(req.body.frst_name == ""){
+        errors.push("Please enter your first name in order to continue");
+        
+    }
+
+    if(req.body.lst_name == ""){
+        errors.push("Please enter your last name in order to continue");
+    }
+
+    
+    if(req.body.psswrd.value == undefined){
+        errors.push("Please enter your last name in order to continue");
+        console.log(errors);
+    }
+
+    if(req.body.usr_rg_eml==""){
+        errors.push("Please Enter you email address in order to continue");
+        
+    }
+
+    if(req.body.brthdy.value==undefined){
+        errors.push("Please Enter your date of birth to continue");
+    }
+
+    if(errors.length > 0 )
+    {
+    res.render("sign_up",{
+        messages:errors
+    })
+}
 })
 
 
